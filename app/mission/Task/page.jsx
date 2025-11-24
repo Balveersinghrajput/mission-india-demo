8eacf5a335c09033848de7bb84521a2f0a85be28
Balveer singh rajput <balveersinghrajput2004@gmail.com>
2025-11-24 08:52:24 +0530

    awd

---
+++ app/mission/Task/page.jsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function TaskPage() {
+  return (
+    <div className="text-white p-10">
+      <h1 className="text-3xl font-bold">Task Page</h1>
+      <p>Mission task content will go here...</p>
+    </div>
+  );
+}
